be54a71273c130abc180d72b3ef8f70dabfd5cf0
lonelyobserver <lcmulaz@gmail.com>
2025-09-20 16:20:09 +0200

    gitbase

--- display_handler.py
+++ display_handler.py
@@ -1,24 +1,9 @@
 import time
-import threading
 from luma.core.interface.serial import spi
 from luma.oled.device import ssd1309
 from luma.core.render import canvas
 from PIL import ImageFont
 
-# Variabile di controllo
-running = True
-
-def input_thread():
-    global running
-    while running:
-        key = input("Premi q e Invio per uscire: ")
-        if key.strip().lower() == "q":
-            running = False
-            break
-
-# Avvia il thread che ascolta la tastiera
-thread = threading.Thread(target=input_thread, daemon=True)
-thread.start()
 
 # Configurazione SPI
 serial = spi(device=0, port=0, gpio_DC=24, gpio_RST=25)
