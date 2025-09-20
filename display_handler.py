bfe26f32ea94966cf5937366518516c9440dcda2
lonelyobserver <lcmulaz@gmail.com>
2025-09-20 16:21:25 +0200

    gitbase

--- display_handler.py
+++ display_handler.py
@@ -10,12 +10,12 @@ serial = spi(device=0, port=0, gpio_DC=24, gpio_RST=25)
 device = ssd1309(serial, width=128, height=64, rotate=0)
 
 # Font
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 2)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 12)
 
 #------------------------------#
 
 counter = 0
-while running:
+while True:
     with canvas(device) as draw:
         draw.text((10, 20), f"Contatore: {counter}", font=font, fill=255)
 
