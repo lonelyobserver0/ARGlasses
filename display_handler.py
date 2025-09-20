ae90adea2bdb45ee1b0db839f29faf810eeffd92
lonelyobserver <lcmulaz@gmail.com>
2025-09-20 16:26:00 +0200

    gitbase

--- display_handler.py
+++ display_handler.py
@@ -4,7 +4,6 @@ from luma.oled.device import ssd1309
 from luma.core.render import canvas
 from PIL import ImageFont
 
-
 # Configurazione SPI
 serial = spi(device=0, port=0, gpio_DC=24, gpio_RST=25)
 device = ssd1309(serial, width=128, height=64, rotate=0)
@@ -15,13 +14,14 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 #------------------------------#
 
 counter = 0
-while True:
-    with canvas(device) as draw:
-        draw.text((10, 20), f"Contatore: {counter}", font=font, fill=255)
+try:
+    while True:
+        with canvas(device) as draw:
+            draw.text((10, 20), f"Contatore: {counter}", font=font, fill=255)
 
-    time.sleep(1)
-    counter += 1
+        time.sleep(1)
+        counter += 1
 
-# Pulizia finale schermo
-device.clear()
-print("Uscito dal programma.")
+except KeyboardInterrupt:
+    device.clear()
+    print("Uscito dal programma.")
